ffc7a12609812186240478cf9bff16172c03bbe5
Nahid Khalili <Nahid_khalili@ymail.com>
2024-11-02 18:34:47 +0330

    ui styles added

--- components/templates/RegistrationForm.js
+++ components/templates/RegistrationForm.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "../styles/form.module.css";
+import styles from "../../styles/form.module.css";
 import Image from "next/image";
 
 const RegistrationForm = () => {
